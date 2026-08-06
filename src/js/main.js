29e104feb0c7425592214928add94ebcec7fe568
RetroAid <111672923+RetroAid@users.noreply.github.com>
2026-08-05 18:13:38 -0600

    Add initCitas function to DOMContentLoaded event

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,11 @@ import '../less/style.less';
 import { initHeaderMenu } from './header.js';
 import { initContactForm } from './contact-form.js';
 import { initActiveNav } from './active-nav.js';
+import { initCitas } from './citas.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     initHeaderMenu();
     initContactForm();
     initActiveNav();
+    initCitas();
 });
